b2d186ed84178a4218089075aba37f4e7dc41899
Pankova Olesya <169178785+OlesyaDmitrievna@users.noreply.github.com>
2024-07-19 17:22:51 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,27 +1,33 @@
+import sys
+
 def find_circular_path(n, m):
-    circular_array = list(range(1, n+1))  # Создаем круговой массив
-    current_index = 0  # Индекс текущего элемента
-    next_index = 3 #Индекс элемента для следующего среза
-    path = []  # Массив для хранения пути
+    circular_array = list(range(1, n + 1))
+    current_index = 0
+    next_index = 3
+    path = []
 
-    while circular_array[next_index]!=1:  # Пока снова не вернемся к 1 элементу
-        next_index = (current_index + m - 1) % n  # Индекс следующего элемента
-        path.append(circular_array[current_index])  # Добавляем текущий элемент в путь
-        current_index = next_index  # Обновляем индекс текущего элемента
-
+    while circular_array[next_index] != 1:
+        next_index = (current_index + m - 1) % n
+        path.append(circular_array[current_index])
+        current_index = next_index
     return path
-def cut_numbers(input_str):
-    numbers = input_str.split()  # Разделение введенной строки по пробелу
 
-    # Теперь у нас есть список строк, представляющих введенные числа
-    # Преобразуем их в числа
+def cut_numbers(input_str):
+    numbers = input_str.split()
+
     if len(numbers) == 2:
-        num1 = int(numbers[0])  # Преобразование первой строки в число
-        num2 = int(numbers[1])  # Преобразование второй строки в число
+        num1 = int(numbers[0])
+        num2 = int(numbers[1])
         return num1, num2
 
-input_str = input('Введите n и m для кругового массива:') # Задаем значение n (размер кругового массива) и m (интервал движения) через пробел
-n, m = cut_numbers(input_str)
+
+if len(sys.argv) != 3:
+    print("Usage: python task1.py <n> <m>")
+    sys.exit(1)
+
+n = int(sys.argv[1])
+m = int(sys.argv[2])
+
 circular_path = find_circular_path(n, m)
 path = ''.join(map(str, circular_path))
 print(path)
